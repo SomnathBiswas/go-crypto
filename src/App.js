b49b5273370eede6225d553dafac78f29affee57
SomnathBiswas <somnathbiswas576@gmail.com>
2023-07-05 15:16:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import { Routes, Route } from 'react-router-dom'
 import Coins from './components/Coins'
 import Coin from './routes/Coin'
 import Navbar from './components/Navbar'
-import DOMPurify from 'dompurify'; 
 
 
 function App() {
     
   const[coins, setCoins]= useState([]);
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en'
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en'
 
   useEffect(()=>{
     axios.get(url).then((response=>{
